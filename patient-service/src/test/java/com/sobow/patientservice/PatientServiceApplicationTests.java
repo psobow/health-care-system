24285f72caf4217b8d66d171c327c4bdb4b31e6c
Patryk Sobow <patryk.sobow95@gmail.com>
2025-10-05 22:32:10 +0200

    Create patient-service

---
+++ patient-service/src/test/java/com/sobow/patientservice/PatientServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.sobow.patientservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientServiceApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+}
