22a63d4ab6fca150310a6457dc9b4323e3a21abe
Patryk Sobow <patryk.sobow95@gmail.com>
2025-10-07 01:25:42 +0200

    Add create patient endpoint and request

--- patient-service/src/main/java/com/sobow/patientservice/controller/PatientController.java
+++ patient-service/src/main/java/com/sobow/patientservice/controller/PatientController.java
@@ -1,12 +1,15 @@
 package com.sobow.patientservice.controller;
 
+import com.sobow.patientservice.dto.PatientRequestDTO;
 import com.sobow.patientservice.dto.PatientResponseDTO;
-import com.sobow.patientservice.model.Patient;
 import com.sobow.patientservice.service.PatientService;
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,10 +20,17 @@ public class PatientController {
     
     private final PatientService patientService;
     
-    
     @GetMapping
-    public ResponseEntity<List<PatientResponseDTO>> getAllPatients(){
+    public ResponseEntity<List<PatientResponseDTO>> getAllPatients() {
         List<PatientResponseDTO> patients = patientService.findAll();
         return ResponseEntity.ok(patients);
+    }
+    
+    @PostMapping
+    public ResponseEntity<PatientResponseDTO> createPatient(
+        @RequestBody @Valid PatientRequestDTO dto
+    ) {
+        PatientResponseDTO response = patientService.create(dto);
+        return ResponseEntity.ok(response);
     }
 }
